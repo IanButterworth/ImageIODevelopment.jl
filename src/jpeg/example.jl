e4c7b2c793fb7246ff49827c5f41014e86a6ced2
Ian <i.r.butterworth@gmail.com>
2019-12-31 15:56:34 -0500

    working Jpeg example

--- src/jpeg/example.jl
+++ src/jpeg/example.jl
@@ -10,47 +10,54 @@ using ColorTypes, ImageCore, FixedPointNumbers
 
 
 #taken from https://stackoverflow.com/questions/9094691/examples-or-tutorials-of-using-libjpeg-turbos-turbojpeg
-function saveimage(img::AbstractArray{T,2}) where T<:Colorant
-    rawimg = rawview(channelview(img))
+function saveimage(filename::String, img::AbstractArray{T,2}; preallocate_max::Bool=false, jpegQual::Int = 93, chromacomp=TJSAMP_420) where T<:Colorant
+    #93 chosen to match imagemagick (identified through https://www.imgonline.com.ua/eng/determine-jpeg-quality-result.php)
+    #420 chosen to match imagemagick http://www.imagemagick.org/Usage/formats/#jpg_write
+
+    rawimg = rawview(channelview(permuteddimsview(img, (2,1))))
     srcBuf = collect(vec(rawimg))
 
     nbands, width, height = size(rawimg)
     pitch = Int(width * nbands)
     pixelFormat = nbands == 3 ? TJPF_RGB : TJPF_GRAY
-    jpegSubsamp = nbands == 3 ? TJSAMP_411 : TJSAMP_GRAY
-    jpegQual = 92
+    jpegSubsamp = nbands == 3 ? chromacomp : TJSAMP_GRAY
     flags = 0
 
     handle = tjInitCompress()
     handle == C_NULL && error("TurboJPEG Error: Unable to initialize compressor object: $(unsafe_string(tjGetErrorStr()))")
 
-    jpegBuf = Ptr{UInt8}(C_NULL)
-    jpegSize = Culong(0)
-    # jpegMaxSize = tjBufSize(width, height, jpegSubsamp)
-    # jpegBuf = Array{UInt8}(undef, jpegMaxSize)
-    # jpegSize = jpegMaxSize
+    jpegSize = preallocate_max ? Ref(tjBufSize(width, height, jpegSubsamp)) : Ref(Culong(C_NULL))
 
-    @show handle, srcBuf, width, pitch, height, pixelFormat, Ref(jpegBuf), Ref(jpegSize), jpegSubsamp, jpegQual, flags
+    jpegBuf = Ref(tjAlloc(jpegSize[]))
 
     tj_stat = tjCompress2(handle, srcBuf, width, pitch, height, pixelFormat,
-                        Ref(jpegBuf), Ref(jpegSize), jpegSubsamp, jpegQual, flags)
+                        jpegBuf, jpegSize, jpegSubsamp, jpegQual, flags)
+
     if tj_stat != 0
         err = tjGetErrorStr()
         tjDestroy(handle)
         handle = C_NULL
         error("TurboJPEG Error: $(unsafe_string(err))")
     end
-    @show jpegSize
 
-    jpegBuf_j = unsafe_wrap(Array, jpegBuf, jpegSize; own = false)
-    open(joinpath(@__DIR__, "out.jpg"), "w") do io
+    jpegBuf_j = unsafe_wrap(Array, jpegBuf[], jpegSize[]; own = false)
+    open(filename, "w") do io
        write(io, jpegBuf_j)
     end
 
     tjstat = tjDestroy(handle) #should deallocate data buffer
     handle = nothing
-    tjFree(jpegBuf)
+    tjFree(jpegBuf[])
+    return nothing
 end
 
+using BenchmarkTools
+img = rand(RGB{N0f8}, 1000, 2000);
+#img = reshape(range(RGB(1,0,0), RGB(0,1,1), length=200), 10, 20)
+@btime saveimage(joinpath(@__DIR__,"out_fileio_imageio.jpg"), $img)
 
-saveimage(rand(RGB{N0f8}, 10, 10))
+using FileIO
+@btime save(joinpath(@__DIR__,"out_fileio_quartimageio.jpg"), $img)
+
+using ImageMagick
+@btime save(joinpath(@__DIR__,"out_fileio_imagemagick.jpg"), $img)
